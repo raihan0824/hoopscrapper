e7ed114a5c8f997b5b2bc7cd0e6ee7f0c0816d7e
raihan0824 <84271135+raihan0824@users.noreply.github.com>
2021-11-04 14:35:56 +0700

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
-from setuptools import setup, find_packages
+import setuptools 
 
-setup(
+setuptools.setup(
     name='hoopscrapper',
     version='1.0.0',
     url='https://github.com/raihan0824/hoopscrapper',
